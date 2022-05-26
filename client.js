3e120582832af1700a2924319e7d7e2972018349
Shaun Wassell <shaun.p.wassell@gmail.com>
2022-05-26 10:50:54 -0400

    Added full game logic

--- client.js
+++ client.js
@@ -6,13 +6,85 @@ let rl = readline.createInterface({
     output: process.stdout,
 });
 
-let socket = socketIoClient('http://127.0.0.1:8080');
+let serverUrl = process.env.IS_DEV
+    ? 'http://127.0.0.1:8080'
+    : 'https://node-tic-tac-toe-shaun.herokuapp.com' 
+
+let socket = socketIoClient(serverUrl);
+
+socket.on('player moves', ({ playerXMoves, playerOMoves }) => {
+    drawGrid(playerXMoves, playerOMoves);
+});
 
 socket.on('your turn', async () => {
-    let response = await rl.question('It\'s your turn now! Please enter your next move: ');
+    let inputValid = false;
+    let response;
+
+    while (!inputValid) {
+        response = await rl.question('It\'s your turn! Please enter your next move: ');
+        inputValid = isValidInput(response);
+        if (!inputValid) {
+            console.log('Invalid input, please enter a capital letter followed by a number.');
+        }
+    }
+
     socket.emit('new move', response);
+});
+
+socket.on('other player turn', () => {
+    console.log('Waiting for the other player\'s input...');
 });
 
 socket.on('info', message => {
     console.log(message);
 });
+
+socket.on('win', () => {
+    console.log('The game is over! You won!!');
+    rl.close();
+    socket.disconnect();
+});
+
+socket.on('lose', () => {
+    console.log('The game is over! You lost!!');
+    rl.close();
+    socket.disconnect();
+});
+
+socket.on('tie', () => {
+    console.log('The game is over! It\'s a tie!!');
+    rl.close();
+    socket.disconnect();
+});
+
+function isValidInput(input) {
+    let [letter, number] = input.split('');
+    return ['A', 'B', 'C'].includes(letter) && ['1', '2', '3'].includes(number);
+}
+
+function drawGrid(xMoves, oMoves) {
+    console.log();
+    drawNumberLabels(),
+    drawVerticalLines(xMoves[0], oMoves[0], 'A');
+    drawHorizontalLine();
+    drawVerticalLines(xMoves[1], oMoves[1], 'B');
+    drawHorizontalLine();
+    drawVerticalLines(xMoves[2], oMoves[2], 'C');
+    console.log();
+}
+
+function drawNumberLabels() {
+    console.log('   1   2   3  ');
+}
+
+function drawVerticalLines(xMoves, oMoves, label) {
+    let space1Char = xMoves[0] ? 'X' : oMoves[0] ? 'O' : ' ';
+    let space2Char = xMoves[1] ? 'X' : oMoves[1] ? 'O' : ' ';
+    let space3Char = xMoves[2] ? 'X' : oMoves[2] ? 'O' : ' ';
+
+    console.log(`${label}  ${space1Char} | ${space2Char} | ${space3Char} `);
+}
+
+function drawHorizontalLine() {
+    console.log('  ---+---+---');
+}
--- server.js
+++ server.js
@@ -8,6 +8,25 @@ let io = new Server(server);
 
 let PORT = process.env.PORT || 8080;
 
+const RUNNING = 'RUNNING';
+const PLAYER_X_WINS = 'PLAYER_X_WINS';
+const PLAYER_0_WINS = 'PLAYER_O_WINS';
+const CATS_GAME = 'CATS_GAME';
+
+let currentPlayer;
+let gameIsOver = false;
+let currentGameState = RUNNING;
+let playerXMoves = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+let playerOMoves = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
 let playerX;
 let playerO;
 
@@ -17,11 +36,118 @@ io.on('connection', socket => {
         playerO = socket;
         playerX.emit('info', 'A second player has joined! Time to start the game!');
         playerO.emit('info', 'You are the second player, the game will now start!');
+
+        startGame();
     } else {
         console.log('The first player has joined, waiting for second player...');
         playerX = socket;
         playerX.emit('info', 'You are the first player, we are waiting for a second player to join...');
     }
+
+    socket.on('new move', input => {
+        let [yMove, xMove] = parseInput(input);
+
+        let currentPlayerMoves = currentPlayer === 'Player X'
+            ? playerXMoves
+            : playerOMoves;
+
+        currentPlayerMoves[yMove][xMove] = 1;
+
+        currentGameState = getNextGameState(playerXMoves, playerOMoves);
+        gameIsOver = [PLAYER_X_WINS, PLAYER_0_WINS, CATS_GAME].includes(currentGameState);
+
+        playerX.emit('player moves', { playerXMoves, playerOMoves });
+        playerO.emit('player moves', { playerXMoves, playerOMoves });
+
+        currentPlayer = currentPlayer === 'Player X' ? 'Player O' : 'Player X';
+
+        if (!gameIsOver) {
+            if (currentPlayer === 'Player X') {
+                playerX.emit('your turn');
+                playerO.emit('other player turn');
+            } else {
+                playerO.emit('your turn');
+                playerX.emit('other player turn');
+            }
+        } else {
+            if (currentGameState === PLAYER_X_WINS) {
+                playerX.emit('win');
+                playerO.emit('lose');
+            } else if (currentGameState === PLAYER_0_WINS) {
+                playerO.emit('win');
+                playerX.emit('lose');
+            } else {
+                playerO.emit('tie');
+                playerX.emit('tie');
+            }
+        }
+    })
 });
+
+function startGame() {
+    console.log('The game has started!');
+    playerX.emit('player moves', { playerXMoves, playerOMoves });
+    playerO.emit('player moves', { playerXMoves, playerOMoves });
+    currentPlayer = Math.random() > 0.5 ? 'Player X' : 'Player O';
+
+    if (currentPlayer === 'Player X') {
+        playerX.emit('your turn');
+        playerO.emit('other player turn');
+    } else {
+        playerO.emit('your turn');
+        playerX.emit('other player turn');
+    }
+}
+
+function parseInput(input) {
+    let [letter, number] = input.split('');
+    return [
+        ['A', 'B', 'C'].indexOf(letter),
+        ['1', '2', '3'].indexOf(number),
+    ]
+}
+
+function getNextGameState(xMoves, oMoves) {
+    let playerXWins = isHorizontalWin(xMoves)
+        || isVerticalWin(xMoves)
+        || isDiagonalWin(xMoves)
+        || isCornersWin(xMoves);
+
+    let player0Wins = isHorizontalWin(oMoves)
+        || isVerticalWin(oMoves)
+        || isDiagonalWin(oMoves)
+        || isCornersWin(oMoves);
+
+    if (playerXWins) {
+        return PLAYER_X_WINS;
+    }
+
+    if (player0Wins) {
+        return PLAYER_0_WINS;
+    }
+
+    return RUNNING;
+}
+
+function isHorizontalWin(moves) {
+    return moves.some(row =>
+        row.every(x => x));
+}
+
+function isVerticalWin(moves) {
+    return [0, 1, 2].some(columnNumber =>
+        moves.every(row => row[columnNumber]));
+}
+
+function isDiagonalWin(moves) {
+    return (moves[0][0] && moves[1][1] && moves[2][2])
+        || (moves[0][2] && moves[1][1] && moves[2][0]);
+}
+
+function isCornersWin(moves) {
+    return moves[0][0] && moves[0][2]
+        && moves[2][0] && moves[2][2];
+}
+
 
 server.listen(PORT, () => console.log('Server is listening on port ' + PORT));
